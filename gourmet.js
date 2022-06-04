64f586304ceef57a67185c319d8960922870fff8
G184552021 <218455@st.takushoku-u.ac.jp>
2022-06-04 10:48:23 +0900

    修正

--- gourmet.js
+++ gourmet.js
@@ -243,16 +243,12 @@ function showResult(resp) {
     console.log(data);
 
     //ここからデータを出力
-    let na = document.querySelector('div#na');
-    for(let i of data.results){
+    
+    for(let i of data.results.shop){
+      let na = document.querySelector('div#na');
       na.textContent = "店舗名 : " + data.results.i.name;
       console.log(na);
-      na.textContent = "営業日時 :" + data.results.i.open;
-      console.log(na);
-      na.textContent = "予算 : " + data.results.i.budget.name;
-      console.log(na);
-      na.textContent = "アクセス情報 : " + data.results.i.access;
-      console.log(na);
+      
     }
 
 
